e4f99872fa2212ab400f13ed9993a2b8e6ca9395
BernardoCosmi <bernardo.daddo90@gmail.com>
2024-01-09 11:28:19 +0100

    calculus fixed

--- main.js
+++ main.js
@@ -1,53 +1,50 @@
 //DEFINIZIONE VARIABILI 
-const nomePasseggero = document.getElementById("name");
-const kmPercorso = document.getElementById("km").value;
-const etaPasseggero = document.getElementById("eta");
-const sendButton = document.getElementById("send-btn");
-const cancelButton = document.getElementById ("cancel-btn");
-const prezzoBase = (kmPercorso * (21/100));
-let prezzoBiglietto;
-let prezzoOver;
-let prezzoUnder; 
+const nomePasseggero = document.getElementById('name');
+const kmPercorso = document.getElementById('km');
+const etaPasseggero = document.getElementById('eta');
+const sendButton = document.getElementById('send-btn');
+const cancelButton = document.getElementById ('cancel-btn');
+let prezzoBiglietto = document.getElementById('price');
+
 
 //Applicazione sconti
-sendButton.addEventListener("click",
+sendButton.addEventListener('click',
     function () {
+        console.log(nomePasseggero.value, kmPercorso.value, etaPasseggero.value,)
         if (etaPasseggero.value <= 18){
-            prezzoUnder = (prezzoBase.value * (80/100));
-            prezzoBiglietto = prezzoUnder.toFixed(2)
-            console.log('Prezzo biglietto under 18 ' + prezzoBiglietto.value + '€');
+            prezzoBiglietto = (((kmPercorso.value)*0.21) * (80/100)).toFixed(2);
+            console.log('Prezzo biglietto under 18 ' + prezzoBiglietto + '€');
             
-            document.getElementById("nome-passeggero").innerHTML = nomePasseggero.value
-            document.getElementById("offerta").innerHTML = "Tariffa ridotta Minorenni"
+            document.getElementById("nome-passeggero").innerHTML = nomePasseggero.value;
+            document.getElementById("offerta").innerHTML = "Tariffa ridotta Minorenni";
             document.getElementById("carrozza").innerHTML = Math.floor(Math.random() * 10) + 1;
             document.getElementById("CP").innerHTML = Math.floor(Math.random() * 99999) + 1;
-            document.getElementById("price").innerHTML = prezzoBiglietto.value
+            document.getElementById("price").innerHTML = prezzoBiglietto + "€";
 
         } else if (etaPasseggero.value >= 65){
-            prezzoOver = (prezzoBase.value * (60/100));
-            prezzoBiglietto = prezzoOver.toFixed(2)
-            console.log('Prezzo biglietto over 65 ' + prezzoBiglietto.value0 + '€');
+            prezzoBiglietto = (((kmPercorso.value)*0.21) * (60/100)).toFixed(2);
+            console.log('Prezzo biglietto over 65 ' + prezzoBiglietto + '€');
             
-            document.getElementById("nome-passeggero").innerHTML = nomePasseggero.value
-            document.getElementById("offerta").innerHTML = "Tariffa ridotta Over 65"
+            document.getElementById("nome-passeggero").innerHTML = nomePasseggero.value;
+            document.getElementById("offerta").innerHTML = "Tariffa ridotta Over 65";
             document.getElementById("carrozza").innerHTML = Math.floor(Math.random() * 10) + 1;
             document.getElementById("CP").innerHTML = Math.floor(Math.random() * 99999) + 1;
-            document.getElementById("price").innerHTML = prezzoBiglietto
+            document.getElementById("price").innerHTML = prezzoBiglietto + "€";
 
         } else{
-            prezzoBiglietto = prezzoBase.toFixed(2)
-            console.log('Prezzo del biglietto non soggetto a sconti ' + prezzoBase.value + '€');
+            prezzoBiglietto = ((kmPercorso.value)*0.21).toFixed(2);
+            console.log('Prezzo del biglietto non soggetto a sconti ' + prezzoBiglietto + '€');
             
-            document.getElementById("nome-passeggero").innerHTML = nomePasseggero.value
-            document.getElementById("offerta").innerHTML = "Tariffa standard"
+            document.getElementById("nome-passeggero").innerHTML = nomePasseggero.value;
+            document.getElementById("offerta").innerHTML = "Tariffa standard";
             document.getElementById("carrozza").innerHTML = Math.floor(Math.random() * 10) + 1;
             document.getElementById("CP").innerHTML = Math.floor(Math.random() * 99999) + 1;
-            document.getElementById("price").innerHTML = prezzoBiglietto
+            document.getElementById("price").innerHTML = prezzoBiglietto + "€";
         }
 })
 
 cancelButton.addEventListener("click", function(){
-    location.reload()
+    location.reload();
 })
 
 
